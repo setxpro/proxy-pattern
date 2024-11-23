92f2907a8e36fedfef4614097a435d02c54b6575
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-11-23 09:55:24 -0300

    fix: modified name

--- src/proxy/ProxyPattern.java
+++ src/proxy/ProxyPattern.java
@@ -1,13 +1,13 @@
 package proxy;
 
-public class ProxyPattener {
+public class ProxyPattern {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(new Client().greet("Patrick"));
-		
+
 	}
-	
+
 }
 
 interface SubjectInterface {
@@ -18,17 +18,16 @@ class RealSubject implements SubjectInterface {
 
 	@Override
 	public String greet(String name) {
-		
+
 		return "Hello, " + name + "!";
 	}
-	
-}
 
+}
 
 class RealSubjectProxy implements SubjectInterface {
 
 	private SubjectInterface realSubject = new RealSubject();
-	
+
 	@Override
 	public String greet(String name) {
 		System.out.println("Proxy: Loggin before greeting...");
@@ -36,7 +35,7 @@ class RealSubjectProxy implements SubjectInterface {
 		System.out.println("Proxy: Loggin after greeting...");
 		return result;
 	}
-	
+
 }
 
 class Client {
